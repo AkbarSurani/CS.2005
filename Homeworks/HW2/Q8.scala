d4ca1cceb7d7661c2419c4bcd1c86fe6121c033b
AkbarSurani <akbar_surani98@yahoo.com>
2016-09-14 04:18:36 +0530

    g

--- Homeworks/HW2/Q8.scala
+++ Homeworks/HW2/Q8.scala
@@ -1,3 +1,12 @@
 object Q8 {
-	
+	def permutationGen(list:List[Int]):List[List[Int]]=
+	{
+		if (list=Nil) Nil
+		else {if (list==hd::rest) {hd::permutationGen(rest), permutationGen(rest)::hd}}
+	}
+}
+
+def main(args:Array[String]):Unit=
+{
+	println("Permutations of [1,4,6,7] are:" + permutationGen([1,4,6,7]))
 }
