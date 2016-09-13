1757e30c83b42c0973b418d7190c910c7dd3602b
AkbarSurani <akbar_surani98@yahoo.com>
2016-09-14 03:18:34 +0530

    k

--- Homeworks/HW2/Q4.scala
+++ Homeworks/HW2/Q4.scala
@@ -1,16 +1,25 @@
 object Q4 {
+
+def sortInsert(list: List[Int], m: Int):List[Int]=
+	list match {
+		case Nil => m::list
+		case hd::rest if m <= hd => m::list
+		case hd::rest if hd < m => hd::sortInsert(rest,m)
+	}
+
 def InsertionSort(list:List[Int]):List[Int]=
 {
-	list match
-	{
-	case Nil => Nil
-	case hd => hd
-	case hd::rest => 	
-		{sortInsert(InsertionSort(rest),hd)
-		}
+	list match {
+		case Nil => Nil
+		case hd::rest => {
+			sortInsert(InsertionSort(rest),hd)
+			}
+		case _ => list
 	}
 }
-}
+
 def main(args: Array[String]): Unit= 
 	{
+		println("Sorted list for (17, 1, 24, 35, 16, 29) is:" + InsertionSort(List(17, 1, 24, 35, 16, 29)))
+}
 }
