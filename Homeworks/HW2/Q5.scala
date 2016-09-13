e3e3284e3b294cd81b697fc1f51fd137a321e7a8
AkbarSurani <akbar_surani98@yahoo.com>
2016-09-14 03:34:41 +0530

    i

--- Homeworks/HW2/Q5.scala
+++ Homeworks/HW2/Q5.scala
@@ -6,14 +6,18 @@ def pivotSort(list:List[Int], x:Int):List[Int]=
 list match 
 {
 case hd::rest => if (hd<x) 
-					lst1::hd;
-					pivotSort(rest, )
-				else lst2::hd
-	
+					lst1=lst1::hd;
+					pivotSort(rest,x)
+				else 
+				lst2=lst2::hd;
+				pivotSort(rest,x)
+case Nil => x	
 }
-	
+var result=lst1::x::lst2
+println(result)	
 }
 }
+
 def main(args: Array[String]): Unit= 
 	{
 }
